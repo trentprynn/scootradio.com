55a96c78a1db4b517e9128d438d4c60b1dee8ffa
trentprynn <trentprynn@gmail.com>
2025-12-06 15:01:55 -0500

    chore: update deps

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    experimental: {
-        typedRoutes: true,
-    },
+    typedRoutes: true,
 };
 
 export default nextConfig;
